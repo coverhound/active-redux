3956d5763b0fab09c71ba4288ae470b814bed903
Bernardo Farah <ber@bernardo.me>
2017-08-01 11:35:21 -0700

    Added model registration in example application

--- example/src/models/Article.js
+++ example/src/models/Article.js
@@ -1,5 +1,7 @@
-import { Model } from '../../dist';
+import { Model, Registry } from '../../dist';
 
 export default class Article extends Model {
-
+  static type = 'articles';
 }
+
+Registry.register(Article);
