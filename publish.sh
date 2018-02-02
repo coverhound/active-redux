9223b059c27da4afafbf923c47d71b24ccb2ea7e
Bernardo Farah <ber@bernardo.me>
2018-02-01 17:00:29 -0800

    Fix publish so it includes dist
    We can verify this is working by running `npm pack` in the local
    directory. Including `src` just so that people running these
    node_modules can have the original reference, though I'm also open to
    the idea of removing that directory.
    
    Added a script for publishing because we can't reliably know that
    `prepublish` is going to only run on publish, and we still want to
    ensure that people remember to run all the steps before publishing.

---
+++ publish.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# Exit on failure of any of these commands
+set -e
+
+# This script is here because we can't safely rely on prepublish only being run
+# on the machine that's publishing.
+# https://blog.greenkeeper.io/what-is-npm-s-prepublish-and-why-is-it-so-confusing-a948373e6be1
+# This is fixed on some versions of NPM, but we can't know what everyone's
+# running, so it's not worth using it.
+
+# Ensure our tests pass and our linter
+npm run lint
+npm test
+
+# Ensure we create our distributed files
+npm run dist
+
+# Publish
+npm publish
